c5f520f476f83e70ad582b72a5a9cdda88a7e3ad
DuyPD <duy.phandinh@meiko.vn>
2022-06-19 00:45:19 +0700

    update

--- main.cpp
+++ main.cpp
@@ -1,8 +1,9 @@
 #include "user.h"
 
-char port[] = "/dev/ttyUSB0";
+char port[] = "/dev/ttyUSB0"; // Ten cong ket noi voi module R305
 
-user _user = user(port);
+user _user = user(port); // Khoi tao doi tuong + ket noi module
+
 string str;
 int num;
 
@@ -15,39 +16,39 @@ int main()
         num = stoi(str);
         if (num == 1)
         {
-            cout << "Quet van tay:\n";
+            cout << "Quet van tay!\n";
             while (1)
             {
-                _user.checkSensor();
-                int i = _user.getBuffer();
-                if (i != BUFFER_EMPTY)
+                _user.checkSensor();       // Goi ham checkSensor lien tuc de quet van tay
+                int i = _user.getBuffer(); // Kiem tra buffer xem co van tay nao duoc phat hien khong
+                if (i != BUFFER_EMPTY)     // Phat hien van tay
                 {
-                    num = _user.getUserID(i);
-                    str = _user.getUserName(num);
-                    cout << "User: " << str << std::endl;
+                    num = _user.getUserID(i);             // Doc ID nguoi dung tu van tay nhap vao
+                    str = _user.getUserName(num);         // Doc ten nguoi dung tu ma nguoi dung
+                    cout << "User: " << str << std::endl; // In ten nguoi dung ra man hinh
                     break;
                 }
             }
         }
         if (num == 2)
         {
-            cout << "Them User:\n";
+            cout << "Them User!\n";
             cout << "Nhap vao ID cua User: ";
             getline(std::cin, str);
             num = stoi(str);
-            cout << "\nNhap vao ho ten cua User: ";
+            cout << "Nhap vao ho ten cua User: ";
             getline(std::cin, str);
-            if (_user.addUser(num, str) < 0)
+            if (_user.addUser(num, str) < 0) // Them nguoi dung vao CSDL voi thong tin duoc nhap tu ban phim.
                 return 0;
             cout << "Them User thanh cong.\n";
         }
         if (num == 3)
         {
-            cout << "Xoa User:\n";
+            cout << "Xoa User!\n";
             cout << "Nhap vao ID cua User: ";
             getline(std::cin, str);
             num = stoi(str);
-            if (_user.deleteUser(num) < 0)
+            if (_user.deleteUser(num) < 0) // Xoa nguoi dung khoi CSDL
                 return 0;
             cout << "Xoa User thanh cong.\n";
         }
